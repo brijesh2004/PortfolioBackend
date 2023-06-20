796fe970295fa5854a3a70a57ad611e248746a87
brijesh2004 <brskumar0102@gmail.com>
2023-06-20 22:32:17 +0530

    adding online database

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose  = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/Portfolio",{
+mongoose.connect("mongodb+srv://brijesh122004:portfolio2023@cluster0.5awcyfk.mongodb.net/?retryWrites=true&w=majority/Portfolio",{
     useNewUrlParser:true
 }).then(()=>{
     console.log("Connection Successfull")
